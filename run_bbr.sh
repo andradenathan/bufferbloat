99ceb06366ce2da012bbce8c5766e934267e6180
0nerb <brenokrangel90@gmail.com>
2024-12-16 15:51:55 -0300

    change run_bbr.sh

--- run_bbr.sh
+++ run_bbr.sh
@@ -14,7 +14,7 @@ iperf_port=5001
 for qsize in 20 100; do
     dir=bb-q$qsize
 
-    python3 bufferbloat.py --bw-net $bwnet --delay $delay --dir $dir --cong $bbr
+    python3 bufferbloat.py --bw-net $bwnet --delay $delay --dir $dir --cong "bbr"
 
     # TODO: Ensure the input file names match the ones you use in
     # bufferbloat.py script.  Also ensure the plot file names match
